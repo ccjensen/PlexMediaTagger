99a337cdfd6ee0d03f5bc3380618e3bb225346e7
Christopher Jensen <christophercjensen@gmail.com>
2015-01-04 21:41:24 -0800

    attempted fix. needs testing

--- PmsRequestHandler.py
+++ PmsRequestHandler.py
@@ -12,8 +12,9 @@ import sys
 import os
 import tempfile
 import platform
-from urllib2 import urlopen, URLError, HTTPError
-from urllib import quote_plus
+import urllib2
+from urllib2 import URLError, HTTPError
+import urllib
 
 class Singleton(object):
     def __new__(type):
@@ -22,16 +23,46 @@ class Singleton(object):
         return type._the_instance
 
 class PmsRequestHandler(Singleton):
-    """docstring for PmsRequestHandler"""
+    """docstring for PmsRequestHandler"""    
     def base_url(self):
         return "http://%s:%s" % (self.ip, self.port)
     #end def base_url
+    
+    def setup_opener(self, username, password):
+        auth_handler = None
+        should_sign_in = len(username) > 0 and len(password) > 0
+        if should_sign_in:
+            sign_in_url = "https://plex.tv/users/sign_in.xml"
+            post_data = urllib.urlencode({"make this": "a POST"})
+            password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            password_manager.add_password(None, sign_in_url, username, password)
+            auth_handler = urllib2.HTTPBasicAuthHandler(password_manager)
+        #end if len(username)
+        self.opener = urllib2.build_opener(auth_handler)
+        self.opener.addheaders = [("X-Plex-Client-Identifier", "PlexMediaTagger")]
+        urllib2.install_opener(self.opener)
+        if should_sign_in:
+            logging.error( "Authenticating user '%s' with plex.tv..." % username )
+            try:
+                xml = urllib2.urlopen(sign_in_url, post_data)
+                contents = ElementTree.parse(xml)
+            except HTTPError, e:
+                logging.critical( "%s", e )
+                sys.exit(1)
+            except IOError, e:
+                logging.debug(e)
+                logging.critical("Could not connect to authenticate user '%s'" % username)
+                sys.exit(1)
+            #end try
+            auth_token = contents.getroot().find("authentication-token").text
+            logging.error( "  Authenticated user '%s' with plex.tv" % username )
+            self.opener.addheaders = [("X-Plex-Token", auth_token)]
     
     def get_contents(self, url):
         logging.debug("Get contents: %s" % url)
         contents = []
         try:
-            xml = urlopen(url)
+            xml = urllib2.urlopen(url)
             contents = ElementTree.parse(xml)
         except IOError, e:
             logging.debug(e)
@@ -58,7 +89,7 @@ class PmsRequestHandler(Singleton):
     
     def download_image(self, partial_url, item_name, desired_item_path):
         full_image_url = "%s%s" %(self.base_url(), partial_url)
-        escaped_image_url = quote_plus(full_image_url)
+        escaped_image_url = urllib.quote_plus(full_image_url)
         transcoder_image_url = "%s/photo/:/transcode?width=%d&height=%d&url=%s" % (self.base_url(), 1980, 1080, escaped_image_url)
         try:
             directory = desired_item_path
@@ -66,7 +97,7 @@ class PmsRequestHandler(Singleton):
                 directory = tempfile.gettempdir()
             #end if not directory
             
-            f = urlopen(transcoder_image_url)
+            f = urllib2.urlopen(transcoder_image_url)
             content_type = f.info()['Content-Type']
             if content_type == 'image/jpeg':
                 image_path = os.path.join(directory, item_name+".jpg")
@@ -95,7 +126,7 @@ class PmsRequestHandler(Singleton):
     def download_stream(self, destination_path, partial_url):
         full_stream_url = "%s%s" %(self.base_url(), partial_url)
         try:
-            f = urlopen(full_stream_url)            
+            f = urllib2.urlopen(full_stream_url)            
             logging.debug( "saving stream to " + destination_path)
             with open(destination_path, "wb") as local_file:
                 local_file.write(f.read())
